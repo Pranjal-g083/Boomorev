05012b172dd048bd9517d8787ab731d7ef8febf3
PRANJAL BARANWAL <77231809+Pranjal-g083@users.noreply.github.com>
2022-06-20 14:24:06 +0530

    included json functionality

--- MovieReview/views.py
+++ MovieReview/views.py
@@ -3,9 +3,17 @@ from multiprocessing import context
 from django.http import HttpResponse
 from .models import Movie, Review
 # Create your views here.
-
+import json
+context = json.load(open('./movie.json','r',encoding="utf8"))
+context_2= json.load(open('./genre.json','r',encoding="utf8"))
+# print( context);
 def home(request):
-    return render(request, 'moviereview/2.html')
+    return render(request, 'moviereview/home.html',context)
 
-def info(request):
-    return render(request, 'moviereview/info.html')
+def info(request,id):
+    for(result) in context['results']:
+        if result['id']==id:
+            return render(request, 'moviereview/info.html',{'result':result,'genres':context_2['genres']})
+    # return render(request, 'moviereview/info.html')
+    else:
+        return HttpResponse('<h1>Not Found</h1>');
