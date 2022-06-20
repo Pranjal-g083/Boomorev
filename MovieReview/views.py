fc7d31e471d8a06d554e89f1d5585c420cd1a910
Prakhar Pandey <3.14prakhar@gmail.com>
2022-06-20 10:36:31 +0530

    Importing movies from json file to the home page and improved on the home page

--- MovieReview/views.py
+++ MovieReview/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from multiprocessing import context
 from django.http import HttpResponse
 from .models import Movie, Review
+import json
+context = json.load(open('./movie.json','r'))
 # Create your views here.
 
 def home(request):
-    return render(request, 'moviereview/2.html')
+    return render(request, 'MovieReview/home.html', context)
 
 def info(request):
-    return render(request, 'moviereview/info.html')
+    return render(request, 'MovieReview/info.html')
