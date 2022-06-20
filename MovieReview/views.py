03702952daa9e0eaddbb91499b684eb6bf7fe0e2
Prakhar Pandey <3.14prakhar@gmail.com>
2022-06-21 00:27:25 +0530

    api key hidden

--- MovieReview/views.py
+++ MovieReview/views.py
@@ -5,17 +5,20 @@ from .models import Movie, Review
 import json
 from urllib.request import urlopen
 
+api_key = json.load(open("api.json","r"))
+ApiKey = api_key["key"]
+
 
 def home(request):
     context = []
-    url = "https://api.themoviedb.org/3/movie/top_rated?api_key=8511985aaf3fd8b644f3956666ae4679&language=en-US&page=1"
+    url = "https://api.themoviedb.org/3/movie/top_rated?api_key=" + ApiKey + "&language=en-US&page=1"
     response = urlopen(url)
     movies = json.load(response)
 
     for movie in movies["results"]:
         url1 = "https://api.themoviedb.org/3/movie/" + \
             str(movie["id"]) + \
-            "?api_key=8511985aaf3fd8b644f3956666ae4679&language=en-US&append_to_response=videos"
+            "?api_key=" + ApiKey + "&language=en-US&append_to_response=videos"
         response = urlopen(url1)
         data = json.load(response)
         context.append(data)
@@ -26,13 +29,13 @@ def home(request):
 def info(request,id):
     url = "https://api.themoviedb.org/3/movie/" + \
             str(id) + \
-            "?api_key=8511985aaf3fd8b644f3956666ae4679&language=en-US&append_to_response=videos"
+            "?api_key=" +ApiKey+"&language=en-US&append_to_response=videos"
     response = urlopen(url)
     movie = json.load(response)
 
     url = "https://api.themoviedb.org/3/movie/" + \
         str(id) + \
-        "/recommendations?api_key=8511985aaf3fd8b644f3956666ae4679&language=en-US"
+        "/recommendations?api_key=" + ApiKey+ "&language=en-US"
     response = urlopen(url)
     recommendations = json.load(response)
     trailer = []
